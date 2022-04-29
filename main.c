8211b971627205de009d37e31a77fa4521dd20e1
mahmut <leipzig9998@hotmail.com>
2022-04-29 18:48:53 +0300

    in line 180 if I use while it prints down-down, if I use for loop it prints in the same line.

--- main.c
+++ main.c
@@ -26,9 +26,8 @@ struct customer {
     struct transaction *trans; // to keep the transactions made by the customer
     struct customer *nextc; //  to point to the next customer in the same branch of the bank
 
-
-
 };
+
 
 struct branch {
     char bname[100];
@@ -92,6 +91,7 @@ void readBranches(struct bank *bank, char *inputFile) {
     }
 
     int index = 1;
+    int bno=1;
     //Create branch
     struct branch *new_branch = malloc(sizeof(struct branch));
 
@@ -102,19 +102,22 @@ void readBranches(struct bank *bank, char *inputFile) {
 
         if (index == 1) {
             memcpy(new_branch->bname, entity, sizeof(entity));
+            new_branch->bno=bno;
+            bno++;
             bank->branches = new_branch;
             new_branch->nextb = malloc(sizeof(struct branch));
             //Go next branch
-            new_branch->nextb=NULL;
+
             new_branch = new_branch->nextb;
             index++;
 
         } else {
             memcpy(new_branch->bname, entity, sizeof(entity));
-
+            new_branch->bno=bno;
+            bno++;
             // Go next branch
             new_branch->nextb = malloc(sizeof(struct branch));
-            new_branch->nextb=NULL;
+
             new_branch = new_branch->nextb;
 
         }
@@ -122,11 +125,35 @@ void readBranches(struct bank *bank, char *inputFile) {
 
     }
 }
+void readCustomers(char *inputFile){
+    FILE *fPtr;
+    char fileNamex[100];
+    strcpy(fileNamex, "C:\\Users\\asxdc\\CLionProjects\\DataProject-1\\");
+
+    strcat(fileNamex, inputFile);
+    fPtr = fopen(fileNamex, "r");
+    if (fPtr == NULL) {
+        printf("There is a error opening the file.");
+
+    }
+    char entity[20];
+    char entity2[20];
+    char entity3[20];
+
+    int iterator=0;
+    while (fscanf(fPtr, "%s %s %s", &entity,&entity2,&entity3) != EOF) {
+
+        // entity ile 2 ikinci branche gidicem
+
+        printf("%d",1);
+    }
+
+}
 
 /* Print all the elements in the linked list */
 void printOpType(struct operation_type *head) {
     struct operation_type *current_node = head;
-    while (current_node != NULL) {
+    while (current_node->nextopt->nextopt != NULL) {
         printf("%s ", current_node->optname);
         printf("%f\n ", current_node->commission);
 
@@ -136,7 +163,7 @@ void printOpType(struct operation_type *head) {
 
 void printBranches(struct bank *head) {
     struct branch *current_node = head->branches;
-    while (current_node != NULL) {
+    while (current_node->nextb->nextb != NULL) {
         printf("%s ", current_node->bname);
 
         current_node = current_node->nextb;
@@ -146,30 +173,21 @@ void printBranches(struct bank *head) {
 
 
 int main() {
-    setbuf(stdout, 0);
-    printf("%s", "1)Read operations types from the file\n");
-    setbuf(stdout, 0);
-    printf("%s", "2)Read branches from the file\n");
-    setbuf(stdout, 0);
-    printf("%s", "3)Read customers from the file\n");
-    setbuf(stdout, 0);
-    printf("%s", "4)Read customer transactions from the file\n");
-    setbuf(stdout, 0);
-    printf("%s", "5)Calculate paid commission amount of each customers in each branches\n");
-
-
-
 
     struct operation_type *headOp = malloc(sizeof(struct operation_type));
     struct bank *headB = malloc(sizeof(struct bank));
 
+    while(1){
+        printf("%s", "1)Read operations types from the file\n");
+        printf("%s", "2)Read branches from the file\n");
+        printf("%s", "3)Read customers from the file\n");
+        printf("%s", "4)Read customer transactions from the file\n");
+        printf("%s", "5)Calculate paid commission amount of each customers in each branches\n");
 
-    int option;
-    scanf("%d", &option);
+        int option;
+        scanf("%d", &option);
 
-
-    switch (option) {
-        case 1:
+        if(option==1){
             setbuf(stdout, 0);
 
             printf("%s", "Please enter the name of the file :");
@@ -179,27 +197,40 @@ int main() {
             readOperationTypes(headOp, filenameOpTypes);
             // Print all operation types which is in headOp
             printOpType(headOp);
+        }
 
-            break;
-        case 2:
+        else if(option==2){
             printf("%s", "Please enter the name of the file :");
             char filenameBank[20];
             scanf("%s", filenameBank);
             readBranches(headB, filenameBank);
             printBranches(headB);
-            break;
 
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
+        }
+        else if(option==3){
+            printf("%s", "Please enter the name of the file :");
+            char filenameBank2[20];
+            scanf("%s", filenameBank2);
+            readCustomers(filenameBank2);
+        }
+        else{
 
-        default:;
+        }
 
     }
 
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
