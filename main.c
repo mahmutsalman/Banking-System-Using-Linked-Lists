ee59e9024ce7e2cde0bd29fdec66faf73ccb1dff
mahmut <leipzig9998@hotmail.com>
2022-05-01 19:11:24 +0300

    option-4 without testing

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ struct bank {
     struct operation_type *optypes;// to hold the transaction types offered by the bank
 };
 
-void readOperationTypes(struct operation_type *operationType, char *inputFile) {
+void readOperationTypes(struct operation_type *headOpType, char *inputFile) {
     FILE *fPtr;
     char fileNamex[100];
     strcpy(fileNamex, "C:\\Users\\asxdc\\CLionProjects\\DataProject-1\\");
@@ -58,6 +58,7 @@ void readOperationTypes(struct operation_type *operationType, char *inputFile) {
     float entity2;
 
     int iterator = 0;// For adding to head
+    int optnum=1;
 
     while (fscanf(fPtr, "%s%f", &entity, &entity2) != EOF) {
         struct operation_type *new_branch = malloc(sizeof(struct operation_type));
@@ -66,21 +67,23 @@ void readOperationTypes(struct operation_type *operationType, char *inputFile) {
         new_branch->commission = entity2;
 
         if (iterator == 0) {
-            operationType->nextopt = NULL;
-            memcpy(operationType->optname, entity, sizeof(entity));
-            operationType->commission = entity2;
+            headOpType->nextopt = NULL;
+            memcpy(headOpType->optname, entity, sizeof(entity));
+            headOpType->commission = entity2;
+            headOpType->optnum=optnum;
             iterator++;
         } else {
-            struct operation_type *lastNode = operationType;
+            struct operation_type *lastNode = headOpType;
             while (lastNode->nextopt != NULL) {
                 lastNode = lastNode->nextopt;
             }
             lastNode->nextopt = new_branch;
+            lastNode->optnum=optnum;
+            optnum++;
         }
 
 
     }
-
 
 }
 
@@ -105,7 +108,7 @@ void readBranches(struct bank *bank, char *inputFile) {
     while (fscanf(fPtr, "%s", &entity) != EOF) {
         //If head is empty, add new branch into it
         if(iterator==0){
-            bank->branches= malloc(sizeof (struct branch));
+            bank->branches= malloc(sizeof (struct branch));//Buna gerek olmayabilir,main içersinde zaten bu yapılıyor
             bank->branches->nextb=NULL;
             memcpy(bank->branches->bname, entity, sizeof(entity));
             bank->branches->bno = bno;
@@ -145,10 +148,16 @@ struct branch  *getProperBranch(struct bank *banka,int entity){
     return NULL;
 
 }
+struct customer *getProperCustomer(struct branch *brancha,int customerId){
+    for(struct customer *cs = brancha->custs; cs;cs=cs->nextc){
+        if(cs->cno==customerId){
+            return cs;
+        }
+    }
+    return NULL;
+}
 
 void readCustomers(char *inputFile,struct bank* bank) {
-    // Creating space for customers
-
     FILE *fPtr;
     char fileNamex[100];
     strcpy(fileNamex, "C:\\Users\\asxdc\\CLionProjects\\DataProject-1\\");
@@ -162,8 +171,8 @@ void readCustomers(char *inputFile,struct bank* bank) {
     int entity;
     char entity2[20];
     char entity3[20];
+    int customerNo=1;
 
-    int iterator = 0;
     while (fscanf(fPtr, "%d %s %s", &entity, &entity2, &entity3) != EOF) {
 
         // entity ile  ikinci branche gidicem,o branch bank head'in içinde
@@ -177,9 +186,11 @@ void readCustomers(char *inputFile,struct bank* bank) {
         struct customer *new_customer= malloc(sizeof (struct customer));
         memcpy(new_customer->fname, entity2, sizeof(entity2));
         memcpy(new_customer->lname, entity3, sizeof(entity3));
+        new_customer->cno=customerNo;
+        customerNo++;
         new_customer->nextc=NULL;
 
-        if(properBranch->custs==NULL){
+        if(properBranch->custs==NULL){ //NEREDE SET LENIYOR BU,BRANCH YARATILIRKEN MI
             properBranch->custs=new_customer;
         }
         else{
@@ -191,12 +202,69 @@ void readCustomers(char *inputFile,struct bank* bank) {
             properBranch->custs->nextc=new_customer;
         }
 
+    }
 
+}
+
+void readTransactions(char *inputFile,struct bank *banka){
+    FILE *fPtr;
+    char fileNamex[100];
+    strcpy(fileNamex, "C:\\Users\\asxdc\\CLionProjects\\DataProject-1\\");
+
+    strcat(fileNamex, inputFile);
+    fPtr = fopen(fileNamex, "r");
+    if (fPtr == NULL) {
+        printf("There is a error opening the file.");
+
+    }
+    int entity; //Branch number
+    int entity2; // Customer id(cid)
+    int entity3; //Operation type
+    float entity4; //transaciton-> amount
+    int tno=1;
+
+    struct customer *temp_customer;
+
+    while (fscanf(fPtr, "%d %d %d %f", &entity, &entity2, &entity3,&entity4) != EOF) {
+
+        //get proper branch
+        struct branch *properBranch = getProperBranch(banka,entity);
+
+        //get proper customer
+        struct customer *properCustomer = getProperCustomer(properBranch,entity2);
+        temp_customer=properCustomer;
+        //Add new transaction to proper place in the customer
+        //If it is first transaction
+        if(properCustomer->trans==NULL){
+            properCustomer->trans= malloc(sizeof (struct transaction));
+            properCustomer->trans->nexttr=NULL;
+            properCustomer->trans->amount=entity4;
+            properCustomer->trans->optype=entity3;
+            tno++;
+        }
+        else{
+            // Create a new transaction
+            struct transaction *new_transaction= malloc(sizeof(struct transaction) );
+            new_transaction->amount=entity4; // Amount
+            new_transaction->optype=entity3; // Opetaion type
+            new_transaction->tno=tno;
+            tno++;
+
+            //Find last transaction
+            while(temp_customer->trans->nexttr!=NULL){
+                temp_customer->trans=temp_customer->trans->nexttr;
+            }
+            //Got last transaction
+            temp_customer->trans=new_transaction;
+
+
+        }
 
 
 
 
     }
+
 
 }
 
@@ -227,7 +295,8 @@ void printBranches(struct bank *head) {
 
 int main() {
 
-    struct operation_type *headOp = malloc(sizeof(struct operation_type));
+    struct operation_type *headOp= malloc(sizeof (struct operation_type));
+    headOp->nextopt=NULL;
     struct bank *headB = malloc(sizeof(struct bank));
 
     headB->branches = malloc(sizeof(struct branch));
@@ -274,8 +343,16 @@ int main() {
             scanf("%s", filenameBank2);
             readCustomers(filenameBank2,headB);
 
-        } else {
+        } else if(option==4) {
+            printf("%s", "Please enter the name of the file :");
+            char filenameBank2[20];
+            scanf("%s", filenameBank2);
+            readTransactions(filenameBank2,headB);
 
+
+        }
+        //option 5
+        else{
 
         }
 
